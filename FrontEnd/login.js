5542ac69434f301c189d45696c3b2a8eadd3c7b6
BOUDRAA Sofian <onyxfour744@gmail.com>
2023-03-14 13:20:41 +0100

    SUppression de la page supplémentaire, gestion de l'affichage connecté ou non grâce à display: none + première version du script de login avec récupération du token

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,3 +1,23 @@
+async function ajoutListenerConnexion() {
+    const formulaireConnexion = document.querySelector(".form-login");
+    formulaireConnexion.addEventListener("submit", async function (e) {
+        e.preventDefault();
+        // création de la variable contenant le mail et le pass de l'utilisateur
+        const user = {
+            email: e.target.querySelector("[name=email").value,
+            password: e.target.querySelector("[name=password").value
+        };
 
+        const chargeUtile = JSON.stringify(user);
+        // appel de la fonction fetch
+        const reponse = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: chargeUtile
+        });
+        console.log((await reponse.json()).token);
+    });
+}
 
+ajoutListenerConnexion();
 
