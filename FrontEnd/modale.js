1e187e9945e50c38e5c704a6edf38647148b3449
sofian <sofian@pop-os.localdomain>
2023-03-24 11:50:37 +0100

    Fin de md1 (visuel)

--- FrontEnd/modale.js
+++ FrontEnd/modale.js
@@ -1,8 +1,8 @@
-import { getProjets } from "./projets";
-
-projets = await getProjets();
-
 let modal = null;
+
+// Récuperation des projets au format JSON
+let reponse = await fetch('http://localhost:5678/api/works/');
+let projets = await reponse.json();
 
 // Fonction permettant d'ouvrir la modale
 const openModal = function (e) {
@@ -35,8 +35,48 @@ const stopPropagation = function (e) {
     e.stopPropagation();
 }
 
+// Fonction permettant de générer la grid de la fenêtre md1 de la modale
+function genererGridMd1 (projets) {
+    for (let i = 0; i < projets.length; i++) {
+        // div globale
+        const divGrid = document.createElement("div");
+        divGrid.id = `div-grid-${i}`;
+        divGrid.classList.add("div-grid");
+
+        // div avec le bouton delete
+        const divGridButtonDelete = document.createElement("div");
+        divGridButtonDelete.className = "div-grid-button-delete";
+        divGridButtonDelete.style.backgroundImage = `url(${projets[i].imageUrl})`;
+
+        const gridButtonDelete = document.createElement("button");
+        gridButtonDelete.id = `grid-button-delete-${i}`;
+        gridButtonDelete.className = "grid-button-delete";
+
+        const iButtonDelete = document.createElement("i");
+        iButtonDelete.className = "fa-solid";
+        iButtonDelete.classList.add("fa-trash-can");
+
+        gridButtonDelete.appendChild(iButtonDelete);
+        divGridButtonDelete.appendChild(gridButtonDelete);
+
+        const editDiv = document.createElement("a");
+        editDiv.id = `${i}`;
+        editDiv.innerText = "éditer"; 
+
+        divGrid.appendChild(divGridButtonDelete);
+        divGrid.appendChild(editDiv);
+        document.querySelector(".grid-md1").appendChild(divGrid);
+    }
+}
+
+genererGridMd1(projets);
+
 // selection de tous les lien permettant d'ouvrir la modal + ajout d'un event permettant de l'ouvrir
 document.querySelectorAll(".js-modal").forEach(a => {
     a.addEventListener("click",openModal);
-    
+});
+
+// PreventDefault sur le lien supprimer tous les projets
+document.querySelector(".delete-all-md1").addEventListener("click", function (e) {
+    e.preventDefault();
 });
